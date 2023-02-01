21874d18700e9c0190b482bd698ad549af64f225
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-02-01 14:33:04 +0800

    canvas drawhouse animation

--- HTML5/canvas/6_image.js
+++ HTML5/canvas/6_image.js
@@ -1,6 +1,6 @@
 const doFirst = () => {
   const canvas = document.querySelector('#canvas')
-  const context = canvas.getContext('2d')
+  context = canvas.getContext('2d')
 
   if (canvas.width > canvas.height) {
     range = canvas.width / 50
@@ -22,13 +22,20 @@ const doFirst = () => {
   context.strokeStyle = 'rgba(0,0,0,0.3)';
   context.stroke();
 
-  let pic = new Image()
-  pic.src = 'https://picsum.photos/300/300?random=1';
-  pic.addEventListener('load', function () {
-    context.drawImage(pic, 0, 0, canvas.width, canvas.height);
-  })
+  pic1 = new Image()
+  pic1.src = 'https://fakeimg.pl/300x300/CCC';
+  pic1.addEventListener('load',drawing)
+  
+  pic2 = new Image()
+  pic2.src = 'https://fakeimg.pl/300x300/CCC';
+  pic2.addEventListener('load',drawing)
 
-
+  const drawing = () => {
+    context.globalAlpha=.5;
+    context.drawImage(pic1, 0, 0, canvas.width, canvas.height);
+    context.globalAlpha=.5;
+    context.drawImage(pic2, 700, 500, 300, 300);
+  }
 }
 
 window.addEventListener('load', doFirst)
---
+++ link.md
@@ -0,0 +1,36 @@
+免費 + 付費圖庫
+https://www.freepik.com/
+https://pixabay.com/
+https://www.freeiconspng.com/
+
+假圖
+https://fakeimg.pl/
+https://loremflickr.com/
+
+codrops
+https://tympanus.net/codrops/
+
+顏色選擇
+kuler
+https://color.adobe.com/zh/explore
+coolors
+https://coolors.co/
+
+學習網站
+https://www.w3schools.com/
+
+email.js
+https://www.emailjs.com/
+
+自動生成信用卡號
+https://www.getcreditcardnumbers.com/
+
+Payment Request API
+https://developer.mozilla.org/en-US/docs/Web/API/Payment_Request_API
+https://blog.techbridge.cc/2017/10/14/payment-request-api/
+
+綠界科技 API
+https://www.ecpay.com.tw/Service/API_Dwnld
+
+專題參考
+https://goo.gl/0ru0Lr
